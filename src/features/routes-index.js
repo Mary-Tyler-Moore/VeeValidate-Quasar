2abe37a476600d631b5e49625115c3ffed52ceee
Tyler Moore <tylermoore@Tylers-iMac.frontierlocal.net>
2020-01-02 08:04:53 -0500

    First Commit

---
+++ src/features/routes-index.js
@@ -0,0 +1,11 @@
+import apcRoutes from './ApiCommunication/route'
+import teRoutes from './TableExtension/route'
+import qtpRoutes from './QuasarThirdParty/route'
+import challenges from './Challenges/routes'
+
+export default {
+  apcRoutes,
+  teRoutes,
+  qtpRoutes,
+  challenges
+}
